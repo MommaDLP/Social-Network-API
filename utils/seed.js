9d3ba9d5c8199c1568bd2e07bbcfe0608e6e0556
MommaDLP <159870485+MommaDLP@users.noreply.github.com>
2024-09-10 20:31:10 -0500

    seed.js

--- utils/seed.js
+++ utils/seed.js
@@ -1,42 +1,42 @@
-const connection = require('../config/connection');
-const { Thought, User} =require('../models');
-const { getRandomEmail, email, getRandomUsername, usernames} =require('./data')
-console.log(getRandomEmail)
-console.log(getRandomUsername)
+const mongoose = require('mongoose');
+const User = require('../models/User');
+const Thought = require('../models/Thought');
 
-connection.on('error', (err) => err);
+//makes user seeds
+const users = [
+    { username: 'apple1234', email:'apple@gmail.com' },
+    { username: 'redhorse456', email: 'redhorse@yahoo.com' },
+    { username: 'lovely678' , email: 'lovely@yahoo.com' },
+    { username: 'turkey789', email:  'turkey@hotmail.com' },
+    { username: 'chargers123', email: 'chargers@yahoo.com' },
+    { username: 'texans0017', email:'texans@gmail.com'},
+    { username: 'loveyou69', email: 'loveyou@yahoo.com'},
+    { username: 'foodie77', email: 'foodie@outlook.com'}
+];
 
-connection.once('open', async () => {
-    let thoughtCheck = await connection.db.listCollections({ name: 'thoughts'}).toArray()
-    if (thoughtCheck.length) {
-        await connection.dropCollection('thoughts');
+//sets up mongo database and seeds it by removing data if it exist and putting new data in
+const MONGO_URI = 'mongodb://localhost:27017/socialNetworkDB';
+
+const seedDB = async () => {
+    try {
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        await User.deleteMany({});
+        await Thought.deleteMany({});
+
+        await User.insertMany(users);
+        console.log('User seed worked!');
+        
+    } catch (error) {
+        console.error('Seed failed...', error);
+    } finally {
+        await mongoose.connection.close();
     }
+};
 
-    let userCheck = await connection.db.listCollections({ name: 'users'}).toArray();
-    if (userCheck.length) {
-        await connection.dropCollection('users');
-    }
-
-
-    const users = [];
-
-    for (let i = 0; i < usernames.length; i++) {
-     
-        const username = getRandomUsername();
-        const email = getRandomEmail();
-
-        users.push({
-            username,
-            email,
-
-        })
-    }
-    console.log("User Array: ", users);
-
-    await User.insertMany(users);
-    console.table(users)
-
-    process.exit(0);
-});
-  
+//actually runs the seed function
+seedDB();
     
